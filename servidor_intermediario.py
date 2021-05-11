f4a61d45537ffc8c1a9534d5d99a14931bf70bdd
NachoLZ <ignacio.lizanaa@sansano.usm.cl>
2021-05-11 16:13:43 -0400

    conectados

--- servidor_intermediario.py
+++ servidor_intermediario.py
@@ -1,29 +1,45 @@
 import socket as skt
+
 SERVERPORT = 55000
+UDPPORT = 50001
+udpSocket = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
+udpSocket.bind(('', SERVERPORT))
 serverSocket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
 serverSocket.bind(("",SERVERPORT))
 serverSocket.listen()
-print("Servidor TCP escuchando en: ",SERVERPORT)
+print("Servidor TCP escuchando en: ", SERVERPORT)
+print("Servidor UDP escuchando en: ", UDPPORT)
 condicion=True
 numero_jugadas=0
-while(condicion==True and numero_jugadas<=3):	
-	clientsocket,clientaddr=serverSocket.accept()
+
+while(condicion == True and numero_jugadas <= 3):	
+
+	clientsocket, clientaddr = serverSocket.accept()
 	print("conectado con: "+ clientaddr[0])
-	msg=clientsocket.recv(2048).decode()
+	msg = clientsocket.recv(2048).decode()
+
+	udpmsg, udpclientAddr = udpSocket.recvfrom(2048)
+	udpmessage = udpmsg.decode()
+	print("Se recibe desde UDP: ", udpmessage)
+	response = "" #respuesta a UDP
+	udpSocket.sendto(response.encode(), udpclientAddr)
+
+
+
 	if(msg=="terminar"):
 		condicion=False
 		clientsocket.close()
 		break;
-	elif(msg=="disponible?"):
-		respuesta="si" #Aqui verificar disponibilidad del servidor cachipun
+	elif(msg=="ready?"):
+		respuesta="OK" #Aqui verificar disponibilidad del servidor cachipun
 		clientsocket.send(respuesta.encode())
-	elif(msg=="papel"):
+	elif(msg=="Papel"):
 		respuesta="has jugado papel  y el bot ha jugado X"#Aqui poner la jugada del servidor cachipun, y enviar el resultado.
 		clientsocket.send(respuesta.encode())
-	elif(msg=="piedra"):
+	elif(msg=="Piedra"):
 		respuesta=" has jugado piedra y etc"
 		clientsocket.send(respuesta.encode())
-	elif(msg=="tijera"):
+	elif(msg=="Tijera"):
 		respuesta=" has jugado tijera y etc"
 		clientsocket.send(respuesta.encode())
 	else:
